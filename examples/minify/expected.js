80a8bc475ac34794b8a940807f4bd63645c8cc3d
bcsgh <33939446+bcsgh@users.noreply.github.com>
2025-04-10 22:49:26 -0700

    Revert examples/minify/expected.js

--- examples/minify/expected.js
+++ examples/minify/expected.js
@@ -1,3 +1,3 @@
-var x={baz_:3,foo_:1,calc:function(){return this.foo_+this.baz_},bar_:2};
+var x={baz_:0,foo_:1,calc:function(){return this.foo_+this.baz_}};x.bar_=2;x["baz_"]=3;
 //! keep this comment
 console.log(x.calc());
